abed2e09cc8c45563d4b326dc56db67fa19874fe
Duncan Temple Lang <duncan@r-project.org>
2023-11-18 15:04:04 -0800

    matchTemplateFunction()

--- R/runTime.R
+++ R/runTime.R
@@ -14,3 +14,25 @@ function(text)
 {
    fromJSON(text, asText = TRUE)
 }
+
+
+matchTemplateFunction =
+function(args, funcNames, append = character(), all = FALSE)
+{
+  cb = sprintf("{%s}", args)
+  m = sapply(cb, function(x) grepl(x, funcNames, fixed = TRUE))
+  ok = apply(m, 1, all)
+  if(!any(ok))
+    stop("cannot find a matching function name")
+  fnNames = funcNames[ok]
+
+  if(length(append))
+     fnNames = grep(sprintf("}/%s$", append), fnNames, value = TRUE)
+
+  if(all)
+    fnNames
+  else
+    fnNames[ which.min(nchar(fnNames)) ]
+}
+
+
